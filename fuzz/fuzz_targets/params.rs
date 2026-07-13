8de5652a69e586a0a3e2c297804c1c9322c60b10
poka <poka@p2p.legal>
2026-07-13 08:33:31 +0200

    chore: thin the upstream workspace to quinn, quinn-proto and quinn-udp

--- fuzz/fuzz_targets/params.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use proto::{Side, transport_parameters::TransportParameters};
-
-fuzz_target!(|data: &[u8]| {
-    let mut data = data;
-    let _ = TransportParameters::read(Side::Client, &mut data);
-});
